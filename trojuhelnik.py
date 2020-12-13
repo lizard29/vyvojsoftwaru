9a29015637c1b36717fbf0eeaee1881d03899116
David Bajer <baj3rdav@gmail.com>
2020-12-13 21:18:54 +0100

    zaokrouhlovani delky strany a obsahu

--- trojuhelnik.py
+++ trojuhelnik.py
@@ -4,16 +4,16 @@ def delkaStrany(x1, y1, x2, y2):
     """Calculate the length of a side of a triangle
     
     Return the length of the side
-    >>> delkaStrany(5, 5, 10, 10) == 8.0
+    >>> delkaStrany(10, 10, 5, 5) == 7.07
     True
     >>> delkaStrany(6, 4, 2, 3)
-    0.75
+    4.12
     """
     str1 = x2-x1
     str2 = y2-y1
     str1 = math.pow(str1, 2)
     str2 = math.pow(str2, 2)
-    st = math.sqrt(str1+str2)
+    st = round(math.sqrt(str1+str2), 2)
     return st
 
 def getObvod(a, b, c):
@@ -24,7 +24,7 @@ def getObsah(a, b, c):
     s = (a + b + c) / 2
     m = s * (s - a) * (s - b) * (s - c)
     if m >= 0:
-        obs = math.sqrt(m)
+        obs = round(math.sqrt(m), 2)
     else:
         obs = -1
     return obs
@@ -55,20 +55,20 @@ def trojuhelnik():
     print("Zadejte souřadnice pro bod C na ose Y: ")
     cy = float(input())
     stra = delkaStrany(cx, cy, bx, by)
-    print("Délka strany a: " + str(round(stra, 2)) + " cm")
+    print("Délka strany a: " + str(stra) + " cm")
     strb = delkaStrany(ax, ay, cx, cy)
-    print("Délka strany b: " + str(round(strb, 2)) + " cm")
+    print("Délka strany b: " + str(strb) + " cm")
     strc = delkaStrany(bx, by, ax, ay)
-    print("Délka strany c: " + str(round(strc, 2)) + " cm")
+    print("Délka strany c: " + str(strc) + " cm")
     if jeSestrojitelny(stra, strb, strc):
         print("Trojúhelník je sestrojitelný")
     else:
         print("Trojúhelník není sestrojitelný")
     obvod = getObvod(stra, strb, strc)
-    print("Obvod trojúhelníku je " + str(round(obvod, 2)) + " cm")
+    print("Obvod trojúhelníku je " + str(obvod) + " cm")
     obsah = getObsah(stra, strb, strc)
     if obsah >= 0:
-        print("Obsah trojúhelníku je " + str(round(obsah, 2)) + " cm")
+        print("Obsah trojúhelníku je " + str(obsah) + " cm")
     else:
         print("Obsah nelze vypočítat (záporné číslo pod odmocninou)")
     if jePravouhly(stra, strb, strc):
